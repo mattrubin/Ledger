b880dfdece48857a0e971063aa6f1f73a7c27bce
Matt Rubin <mattrubin@me.com>
2013-05-26 22:34:39 -0700

    Refactored Pages spec

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,55 +2,53 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ledger" }
+
   describe "Home page" do
+  before { visit '/pages/home' }
 
     it "should have the content 'Ledger'" do
-      visit '/pages/home'
       expect(page).to have_content('Ledger')
     end
 
     it "should have the title 'Home'" do
-      visit '/pages/home'
-      expect(page).to have_title("Ledger | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
   describe "Help page" do
+    before { visit '/pages/help' }
 
     it "should have the content 'Help'" do
-      visit '/pages/help'
       expect(page).to have_content('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/pages/help'
-      expect(page).to have_title("Ledger | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
   describe "About page" do
+    before { visit '/pages/about' }
 
     it "should have the content 'About Ledger'" do
-      visit '/pages/about'
       expect(page).to have_content('About Ledger')
     end
 
     it "should have the title 'About Us'" do
-      visit '/pages/about'
-      expect(page).to have_title("Ledger | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
 
   describe "Contact page" do
+    before { visit '/pages/contact' }
 
     it "should have the content 'Contact'" do
-      visit '/pages/contact'
       expect(page).to have_content('Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/pages/contact'
-      expect(page).to have_title("Ledger | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
