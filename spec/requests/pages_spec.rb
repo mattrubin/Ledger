3ba8b30c28dbe8217665f34f0ac896cb08ec742a
Matt Rubin <mattrubin@me.com>
2013-05-26 22:06:52 -0700

    Stubbed about page

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -17,4 +17,12 @@ describe "Static pages" do
       expect(page).to have_content('Help')
     end
   end
+
+  describe "About page" do
+
+    it "should have the content 'About Ledger'" do
+      visit '/pages/about'
+      expect(page).to have_content('About Ledger')
+    end
+  end
 end
