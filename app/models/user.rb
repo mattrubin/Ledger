bea54756a5238f9ca1e986ac424014ba192bdc98
Matt Rubin <mattrubin@me.com>
2013-05-28 20:59:07 -0700

    Use usernames as the parameter for user_path instead of the UUID

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,8 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password_confirmation, presence: true
   validates :password, length: { minimum: 10 }
+
+  def to_param # overridden
+    username
+  end
 end
