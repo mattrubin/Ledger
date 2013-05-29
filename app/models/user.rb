f0fcf74830334e67ff6cd3b30e99a40d1b187bb8
Matt Rubin <mattrubin@me.com>
2013-05-29 00:19:55 -0700

    Successful sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   	email.downcase!
   	username.downcase!
   end
+  before_save :create_remember_token
+
 
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -22,4 +24,11 @@ class User < ActiveRecord::Base
   def to_param # overridden
     username
   end
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
+
 end
