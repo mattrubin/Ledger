93941733996ca7f44225e495705a524e9196e102
Matt Rubin <mattrubin@me.com>
2013-05-28 22:37:42 -0700

    Success flash on signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to Ledger!"
       redirect_to @user
     else
       render 'new'
