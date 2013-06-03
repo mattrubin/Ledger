4534492227da516a3aac8bf938b1d026726b1d87
Matt Rubin <mattrubin@me.com>
2013-06-02 22:31:08 -0700

    Retina-ready Gravatars

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
   def gravatar_for(user, options = { size: 50 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size*2}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar", size: "#{size}x#{size}")
   end
 end
